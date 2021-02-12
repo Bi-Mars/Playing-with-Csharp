8e2dce9253cdcd8627a5de3284845747afbbd4a9
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-02-11 22:20:25 -0500

    Creating own libraries and using them

---
+++ MyLibrary/MyLibrary/MyTimer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MyLibrary
+{
+    public class MyTimer
+    {
+        //declare background worker
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private int tInterval;
+
+        //delegate and event
+        public delegate void timerTick();
+        //event
+        public event timerTick tick;
+        public MyTimer()
+        {
+            backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker.DoWork += new DoWorkEventHandler(DoWork);
+            backgroundWorker.WorkerSupportsCancellation = true;
+            tInterval = 100; // sleeping for 100 millisecond
+
+
+
+        }
+
+        //property of tickInterval
+        public int tickInt
+        {
+            get
+            {
+                return tInterval;
+            }
+            set
+            {
+
+                tInterval = value;
+            }
+        }
+
+        //start button
+        public void Start()
+        {
+            if (backgroundWorker.IsBusy == false)
+            {
+                backgroundWorker.RunWorkerAsync(); // run the background worker
+            }
+        }
+
+
+        //method
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            //what to do in background
+            // Go to Sleep:
+            while (backgroundWorker.CancellationPending == false)
+            {
+                // sleeping for tInterval
+                System.Threading.Thread.Sleep(tInterval);
+                //call tick event
+                tick();
+
+            }
+
+        }
+
+        public void Stop()
+        {
+            backgroundWorker.CancelAsync();
+        }
+    }
+}
