3b5b36fac3d8ad18880debf361b98a946530af17
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-02-11 22:23:33 -0500

    Server Program

---
+++ ServerProgram/ServerProgram/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ServerProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.bgConnect = new System.ComponentModel.BackgroundWorker();
+            this.bgSocket = new System.ComponentModel.BackgroundWorker();
+            this.richResponse = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(21, 233);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(532, 22);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyUp);
+            // 
+            // bgConnect
+            // 
+            this.bgConnect.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgConnect_DoWork);
+            this.bgConnect.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgConnect_RunWorkerCompleted);
+            // 
+            // bgSocket
+            // 
+            this.bgSocket.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgSocket_DoWork);
+            this.bgSocket.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgSocket_RunWorkerCompleted);
+            // 
+            // richResponse
+            // 
+            this.richResponse.Location = new System.Drawing.Point(12, 23);
+            this.richResponse.Name = "richResponse";
+            this.richResponse.Size = new System.Drawing.Size(541, 177);
+            this.richResponse.TabIndex = 2;
+            this.richResponse.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richResponse);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.ComponentModel.BackgroundWorker bgConnect;
+        private System.ComponentModel.BackgroundWorker bgSocket;
+        private System.Windows.Forms.RichTextBox richResponse;
+    }
+}
+
---
+++ ServerProgram/ServerProgram/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using network
+using System.Net.Sockets; // create sockets
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ServerProgram
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server; // listen to somebody to connects
+        private TcpClient client;
+
+        private NetworkStream ns; // IO --> read and write using network stream
+        StreamReader sr; // 
+        StreamWriter sw;
+        Boolean open; // can i take other connection?
+        String s;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //port number 18888
+            constructorRebuilt();
+           
+
+        }
+
+        void constructorRebuilt()
+        {
+            try // try becoming client if throws error the become server
+            {
+                client = new TcpClient();
+                client.Connect(IPAddress.Loopback, 18888);
+                ns = client.GetStream();
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+                //  open = true; // connection is false
+                bgSocket.RunWorkerAsync(); // listen
+                open = true; // client is connected
+                this.Text = "Client"; // form name
+            }
+            catch (Exception ee)
+            {
+                server = new TcpListener(IPAddress.Loopback, 18888); // who you wanna connect to? .Loopback --> talk to same computer
+                open = false; // connection is false
+
+                bgConnect.RunWorkerAsync(); // listen
+                server.Start();
+                this.Text = "Server";
+            }
+            //run in the background 
+        }
+        private void bgConnect_DoWork(object sender, DoWorkEventArgs e)
+        {
+            client = server.AcceptTcpClient(); // in background accept the client request
+        }
+
+        //connection.
+        private void bgConnect_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ns = client.GetStream();
+            sr = new StreamReader(ns); 
+            sw = new StreamWriter(ns);
+            open = true; // we have connection 
+            server.Stop(); // stops connection because we only have 1 conncetion
+            bgSocket.RunWorkerAsync(); // after connection has been established, listen to the client
+        }
+
+        private void bgSocket_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // while reading is error is encountered 
+            try
+            {
+                s = sr.ReadLine();
+            } catch(Exception ee)
+            {
+                //close everything
+                sw.Close();
+                sr.Close();
+                ns.Close();
+                client.Close();
+                server.Stop();
+                open = false;
+            }
+        }
+
+        private void bgSocket_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(s == "Quit")
+            {
+                this.Close();
+                return;
+            }
+
+            if(!open) // if no connection
+            {
+                return;
+            }
+            //set a selection for a change of color
+            //change the color. Our selection will be at the end of the current text
+            //change color and then add the new text
+
+            richResponse.SelectionStart = richResponse.TextLength; // starting position
+            richResponse.SelectionLength = 0;
+
+            if(this.Text == "Server")
+            {
+                richResponse.SelectionColor = Color.Red;
+                richResponse.AppendText("Sent from Client:  " + s + "    Sent at: " + DateTime.Now.ToString() + "\n" );
+               // richResponse.Text = richResponse.Text + "Sent from Client: " + s + "\n"; // this text gets transmitted
+            }
+            else
+            {
+                richResponse.SelectionColor = Color.Green;
+                richResponse.AppendText("Sent from Server:   " + s + "    Sent at: " + DateTime.Now.ToString() + "\n");
+               // richResponse.Text = richResponse.Text + "Sent from Server: " + s + "\n";
+            }
+           
+            bgSocket.RunWorkerAsync(); // listen again
+            
+        }
+
+     /*   private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(open)
+            {
+                lblResponse.Text = lblResponse.Text + "Sent from " + this.Text + " :" + txtMessage.Text + "\n";
+                sw.WriteLine(txtMessage.Text);
+                txtMessage.Text = "";
+                sw.Flush(); // flush the buffer writer
+            }
+            
+        }
+        */
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(open == true)
+            {
+                sw.WriteLine("Quit");
+                sw.Close(); // only opens when connection has been established
+                sr.Close();
+                ns.Close();
+                client.Close();
+
+            }
+
+            // server.Stop();
+            bgConnect.Dispose(); // stop the process
+            bgSocket.Dispose();
+            open = false;
+        }
+
+        private void txtMessage_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if(!open)
+            {
+                return;
+            }
+            if(e.KeyCode == Keys.Return) // keyEventArgs will tell what key is entered
+            {
+                richResponse.SelectionStart = richResponse.TextLength;
+                richResponse.SelectionLength = 0;
+
+                if(this.Text == "Client")
+                {
+                    richResponse.SelectionColor = Color.Red;
+                   richResponse.AppendText("Sent from " + this.Text + " :" + txtMessage.Text + "    Sent at: " + DateTime.Now.ToString() + "\n");
+                }
+
+                else
+                {
+                    richResponse.SelectionColor = Color.Green;
+                    richResponse.AppendText("Sent from " + this.Text + " :" + txtMessage.Text + "    Sent at: " + DateTime.Now.ToString() + "\n");
+                }
+                 //   richResponse.Text = richResponse.Text + "Sent from " + this.Text + " :" + txtMessage.Text + "\n";
+                    sw.WriteLine(txtMessage.Text);
+                    txtMessage.Text = "";
+                    sw.Flush(); // flush the buffer writer
+                
+                  
+            }
+        }
+
+        // close file all the resources 
+    }
+}
