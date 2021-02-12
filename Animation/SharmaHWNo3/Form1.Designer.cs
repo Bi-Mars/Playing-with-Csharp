07bd8caffc0f10e94a410c950f2a55d6f9a6d0ab
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-02-11 22:09:30 -0500

    Animation with C#

---
+++ Animation/SharmaHWNo3/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SharmaHWNo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.Faster = new System.Windows.Forms.Button();
+            this.Slower = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, -1);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(365, 437);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // Faster
+            // 
+            this.Faster.Location = new System.Drawing.Point(49, 461);
+            this.Faster.Name = "Faster";
+            this.Faster.Size = new System.Drawing.Size(101, 36);
+            this.Faster.TabIndex = 1;
+            this.Faster.Text = "Faster";
+            this.Faster.UseVisualStyleBackColor = true;
+            this.Faster.Click += new System.EventHandler(this.Faster_Click);
+            // 
+            // Slower
+            // 
+            this.Slower.Location = new System.Drawing.Point(215, 461);
+            this.Slower.Name = "Slower";
+            this.Slower.Size = new System.Drawing.Size(101, 36);
+            this.Slower.TabIndex = 2;
+            this.Slower.Text = "Slower";
+            this.Slower.UseVisualStyleBackColor = true;
+            this.Slower.Click += new System.EventHandler(this.Slower_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(400, 116);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(101, 36);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(400, 199);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(101, 36);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(712, 509);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Slower);
+            this.Controls.Add(this.Faster);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button Faster;
+        private System.Windows.Forms.Button Slower;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.ComponentModel.BackgroundWorker sender;
+    }
+}
+
---
+++ Animation/SharmaHWNo3/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyLibrary; // MyTimer class is in this library in .dll
+
+namespace SharmaHWNo3
+{
+    public partial class Form1 : Form
+    {
+        //declare variables
+        Image[] img;
+        int picNo;
+        int numPics;
+        MyTimer mytimer;
+        public Form1()
+        {
+            InitializeComponent();
+         
+            picNo = 0;
+            numPics = 5;
+
+            //initialize the array
+            img = new Image[numPics];
+
+            for(int i = 1; i < numPics; i++)
+            {
+                img[i-1] = Image.FromFile(getPath() + "Position" + i + ".png");
+            }
+
+            pictureBox.Image = img[0];
+
+            //initialize timer class
+            mytimer = new MyTimer();
+            // update the event
+            mytimer.tick += new MyTimer.timerTick(timerTick);
+        }
+
+      void  timerTick()
+        {
+            picNo++;
+            picNo = picNo % numPics;
+            pictureBox.Image = img[picNo];
+        }
+
+
+        string getPath()
+        {
+            String s;
+            int i;
+            s = Application.StartupPath; // this is the path used when application begins
+            i = s.IndexOf(Application.ProductName);
+            s = s.Substring(0, i + Application.ProductName.Length + 1); // this will get the location where animation.sln is 
+            return s;
+        }
+
+        private void Faster_Click(object sender, EventArgs e)
+        {
+            //sleep shorter
+            mytimer.tickInt = 50;
+        }
+
+        private void Slower_Click(object sender, EventArgs e)
+        {
+            // sleep longer
+            mytimer.tickInt = 200;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            mytimer.Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            mytimer.Stop();
+        }
+    }
+}
