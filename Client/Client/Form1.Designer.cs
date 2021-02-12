07faff2e8436ef7a3361861a204cb158f63674a4
Bimarsh Sharma <bimarshsharma101@gmail.com>
2021-02-11 22:12:05 -0500

    Listening to Client request using TcpListener

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResponse = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.bgSocket = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // lblResponse
+            // 
+            this.lblResponse.Location = new System.Drawing.Point(194, 64);
+            this.lblResponse.Name = "lblResponse";
+            this.lblResponse.Size = new System.Drawing.Size(319, 180);
+            this.lblResponse.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(197, 262);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(252, 22);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(271, 314);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // bgSocket
+            // 
+            this.bgSocket.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgSocket_DoWork);
+            this.bgSocket.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgSocket_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblResponse);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResponse;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.ComponentModel.BackgroundWorker bgSocket;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using network
+using System.Net.Sockets; // create sockets
+using System.Net;
+using System.IO;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+       // private TcpListener server; // listen to somebody to connects
+        private TcpClient client;
+
+        private NetworkStream ns; // IO --> read and write using network stream
+        StreamReader sr; // 
+        StreamWriter sw;
+       // Boolean open; // can i take other connection?
+        String s;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 18888);
+
+            ns = client.GetStream();
+            sr = new StreamReader(ns);
+            sw = new StreamWriter(ns);
+          //  open = true; // connection is false
+
+            bgSocket.RunWorkerAsync(); // listen
+            //server.Start();
+
+            //run in the background 
+        }
+
+        private void bgSocket_DoWork(object sender, DoWorkEventArgs e)
+        {
+            s = sr.ReadLine();
+        }
+
+        private void bgSocket_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblResponse.Text = lblResponse.Text + s;
+            bgSocket.RunWorkerAsync();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            sw.WriteLine(txtMessage.Text + "\n");
+            txtMessage.Text = "";
+            sw.Flush();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+         
+                //close everything
+                sw.Close();
+                sr.Close();
+                ns.Close();
+                client.Close();
+        }
+    }
+}
